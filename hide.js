b4f27851d6c80a80bbda157313a3d6b12fa1cf73
Alim Giray Aytar <alimgiray@gmail.com>
2021-02-07 19:55:48 +0300

    Implement basic functionality - Add users to list - Remove users from list - Render posts according to hidden users list

---
+++ hide.js
@@ -0,0 +1,46 @@
+// Hides posts with given users on the page
+function hidePosts() {
+  chrome.storage.sync.get(['hiddenUsers'], function(result) {
+    var posts = window.document.getElementsByClassName("entry0");
+    for (var post of posts) {
+      for (var i = 0; i < post.childNodes.length; i++) {
+        if (post.childNodes[i].className == "bottomright duclsact") {
+          usernameDiv = post.childNodes[i];
+          username = usernameDiv.childNodes[0].childNodes[0].text;
+          post.style.display = "block";
+          if (result.hiddenUsers.includes(username)) {
+            post.style.display = "none";
+          }
+          break;
+        }        
+      }
+    }
+  });
+}
+
+// Add a new item in the store
+function addHiddenUser() {
+  chrome.storage.sync.get(['hiddenUsers'], function(result) {
+    var userToHide = document.getElementById("hiddenUsersInput").value;
+    if (!result.hiddenUsers.includes(userToHide)) {
+      result.hiddenUsers.push(userToHide);
+      chrome.storage.sync.set({"hiddenUsers": result.hiddenUsers}, function() {});
+      createList();
+    }
+    document.getElementById("hiddenUsersInput").value = "";
+  });
+}
+
+function removeHiddenUser(username) {
+  chrome.storage.sync.get(['hiddenUsers'], function(result) {
+    const hiddenUsers = result.hiddenUsers.filter(user => user !== username);
+    chrome.storage.sync.set({"hiddenUsers": hiddenUsers}, function() {});
+    createList();
+  });
+}
+
+hidePosts();
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  hidePosts();
+});
